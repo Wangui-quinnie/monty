4171ab56995abac6fba119b823a956b115279b7e
Wangui-quinnie <w.wangui@ymail.com>
2023-08-24 08:36:35 -0700

    pop

---
+++ op_pop.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+  *pop - implements the pop opcode
+  *@stack: pointer to the stack
+  *@line_number: line number in the file
+  */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(temp);
+}
